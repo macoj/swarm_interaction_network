e32d7a1dfb36095cc1276982414145ce0ebb2ec0
Marcos Oliveira <moliveirajun2013@my.fit.edu>
2015-11-23 12:00:52 -0500

    Fixed error in the calculation of R from Worasucheep

--- PSO/src/pso/AnalysisWorasucheep.java
+++ PSO/src/pso/AnalysisWorasucheep.java
@@ -6,38 +6,40 @@ package pso;
 
 import java.io.PrintWriter;
 import java.util.LinkedList;
+/*
+ CHECKS:
+	OK 
+*/
+
 
 // "A Particle Swarm Optimization with stagnation detection and dispersion"
 // Evolutionary Computation, 2008. CEC 2008. (IEEE World Congress on Computational Intelligence). IEEE Congress on
 // Worasucheep, C.
 
-/*
- NOT CHECKED 
-*/
 public class AnalysisWorasucheep {
 	
 	private PSO pso;
 	private PrintWriter printer;
 	private LinkedList<Double> window = new LinkedList<Double>();
-	private double previousFbest;
-	private double currentFbest;
-	private double avg_particles_velocity;
-	private double previous_avg_particles_velocity;
+	private double previous_best_value;
+	private double current_best_value;
+	private double current_average_particles_velocity;
+	private double previous_average_particles_velocity;
 	private double ratio;
 	private final int WINDOW_SIZE = 500;
 	private final int CONVERGENCE_CHECK = 1;
 	
-        public AnalysisWorasucheep(PSO pso, PrintWriter printer){
+    public AnalysisWorasucheep(PSO pso, PrintWriter printer) {
 		this.pso = pso;
 		this.printer = printer;
 		ratio = Double.NaN;
 	}
         
 	public double getAvg_particles_velocity() {
-		return avg_particles_velocity;
+		return current_average_particles_velocity;
 	}
 	public double getPrevious_avg_particles_velocity() {
-		return previous_avg_particles_velocity;
+		return previous_average_particles_velocity;
 	}
 
 	public double getR() {
@@ -45,113 +47,89 @@ public class AnalysisWorasucheep {
 	}
 
 	public double getCurrentFbest() {
-		return currentFbest;
+		return current_best_value;
 	}
 	
 	public double getPreviousFbest(){
-		return this.previousFbest;
+		return this.previous_best_value;
 	}
 
-	public void detectStagnation(){
-		
-		if(pso.current_iteration>=WINDOW_SIZE){
-			double fc = currentFbest;
-			double fp = previousFbest;
-			double vc = avg_particles_velocity;
-			double vp = getAverageVelocityByWindow();
-			
-			
-			ratio = Math.abs(1-(fc/fp)/1-(vc/vp));
-			if(ratio<Math.pow(10, -5)){
-				//runDispersion();
-			}
+	public void evaluateRatio(){
+		if(pso.current_iteration >= WINDOW_SIZE){
+			double fc = current_best_value;
+			double fp = previous_best_value;
+			double vc = current_average_particles_velocity;
+			double vp = getAverageVelocityFromWindow();
+			ratio = (1 - fc/fp)/(1 - vc/vp);
+			ratio = Math.abs(ratio);
 		}
-		
 	}
 
-	private void runDispersion() {
-		// In order to perform this action, it would be necessary to change the "updateVelocity" method in the PSO class,
-		// making it to receive parameters.
-	}
-	
-	private double getAvgVelocityOfAllParticles(){
-		
+	private double evaluateParticlesAverageVelocity(){
 		double sum;
-		double[] avg_by_particle = new double[this.pso.DIMENSION];
+		double[] norms_of_particles_velocities = new double[this.pso.DIMENSION];
 		
+		previous_average_particles_velocity = current_average_particles_velocity;
 		
-		// average velocity of all particles
-		for (int i=0; i<this.pso.particle_velocity.length; i++){   
-			
-//			sum = 0;
-//			for(int j=0; j<this.pso.DIMENSION; j++){
-//				sum += this.pso.particle_velocity[i][j];
-//			}
-//			
-//			avg_by_particle[i] = sum/this.pso.DIMENSION;
-                    
-                    sum = 0;
-                    for(int j=0; j<this.pso.DIMENSION; j++){
-			sum += Math.pow(this.pso.particle_velocity[i][j], 2);
-                    }
-			
-                    avg_by_particle[i] = Math.sqrt(sum);
-			
+		for (int particle = 0; particle < this.pso.NUMBER_OF_PARTICLES; particle++) {   
+            sum = 0d;
+            for(int dimension = 0; dimension < this.pso.DIMENSION; dimension++) {
+            	sum += Math.pow(this.pso.particle_velocity[particle][dimension], 2);
+            }
+            norms_of_particles_velocities[particle] = Math.sqrt(sum);
 		}
 		
-		sum=0;
-		for(int i=0; i<this.pso.particle_velocity.length; i++){
-			sum += avg_by_particle[i];
+		sum = 0d;
+		
+		for (int particle = 0; particle < this.pso.NUMBER_OF_PARTICLES; particle++) {
+			sum += norms_of_particles_velocities[particle];
 		}
 		
-		previous_avg_particles_velocity = avg_particles_velocity;
+		current_average_particles_velocity = sum/((double) this.pso.NUMBER_OF_PARTICLES);
 		
-		avg_particles_velocity = sum/this.pso.particle_velocity.length;
-		
-		if(pso.current_iteration<WINDOW_SIZE)
-			window.add(avg_particles_velocity);
-		else{
-			window.poll();
-			window.add(avg_particles_velocity);
+		if(pso.current_iteration < WINDOW_SIZE) {
+			window.add(current_average_particles_velocity);
+		} else {
+			window.poll();  // is the running time constant?  
+			window.add(current_average_particles_velocity);
 		}
 		
-		if(this.pso.current_iteration==0)
-			previous_avg_particles_velocity = Double.NaN;
-		
-		return avg_particles_velocity;
+		if (this.pso.current_iteration == 0) {
+			previous_average_particles_velocity = Double.NaN;
+		}
+			
+		return current_average_particles_velocity;
 	}	
 	
 	private void updateBestSwarmFitness(){
-		previousFbest = currentFbest;
-		currentFbest = pso.particle_best_value[pso.swarm_gbest];
-		if(pso.current_iteration==0)
-			previousFbest = Double.NaN;
+		previous_best_value = current_best_value;
+		current_best_value = pso.particle_best_value[pso.swarm_gbest];
+		if (pso.current_iteration == 0) {
+			previous_best_value = Double.NaN;
+		}
 	}
 	
-	private void updatePreviousBestSwarmFitness(){
-		this.previousFbest = this.currentFbest;
-	}
-	
-	private double getAverageVelocityByWindow(){
-		double sum = 0;
+	private double getAverageVelocityFromWindow(){
+		double sum = 0d;
 		double averageVelocityByWindow;
 		
-		for(int i=0; i<window.size(); i++){
+		for (int i = 0; i < this.window.size(); i++) {
 			sum += window.get(i);
 		}
 		
-		averageVelocityByWindow = sum/window.size();
+		averageVelocityByWindow = sum/((double) this.WINDOW_SIZE);
 		
 		return averageVelocityByWindow;
 	}
 	
 	public void iterate(){
-		this.getAvgVelocityOfAllParticles();
-		updateBestSwarmFitness();
+		this.evaluateParticlesAverageVelocity();
+		this.updateBestSwarmFitness();
 		String rLine = "R:#" + this.pso.current_iteration + " " + ratio;
 		printer.println(rLine + "\n");
-		if(pso.current_iteration%CONVERGENCE_CHECK==0)
-			this.detectStagnation();
+		if (pso.current_iteration%CONVERGENCE_CHECK == 0) {
+			this.evaluateRatio();
+		}
 				
 	}
 }
