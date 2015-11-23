2125528c11cab0a4c3e9202069b7da2213b37cfa
Marcos Oliveira <moliveirajun2013@my.fit.edu>
2015-11-23 15:55:43 -0500

    improving perfomance by extracting method and keeping the results.

--- PSO/src/pso/AnalysisOlorunda.java
+++ PSO/src/pso/AnalysisOlorunda.java
@@ -10,158 +10,130 @@ CHECKS:
 	NONE 
 */
 
-
 // "Measuring exploration/exploitation in particle swarms using swarm diversity"
 // Evolutionary Computation, 2008. CEC 2008. (IEEE World Congress on Computational Intelligence). IEEE Congress on
 // Olorunda, O. ; Engelbrecht A.P.
+
+/*
+ (1) The diameter of a swarm is defined as the maximum distance 
+     between any 2 particles in the swarm.
+ (2) the radius can be calculated as the distance between the 
+     swarm center and the particle in the swarm which is furthest 
+     away from it.
+ */
 public class AnalysisOlorunda {
 
 	private PSO pso;
 	private PrintWriter printer;
 	private double diameter;
 	private double radius;
-	private double avgAroundCenter;
-	private double avgOfAvg;
-	private double[] center; // To define what is the swarm center
+	private double average_distance_around_center;
+	private double normalized_average_distance_around_center;
+	private double average_distance_among_particles;
+	private double[] spatial_swarm_center; 
+	private double[] distances_to_center;
+	private double[][] distances_among_particles;
 
 	public AnalysisOlorunda(PSO pso, PrintWriter printer) {
 		this.pso = pso;
 		this.printer = printer;        
-		center = new double[pso.DIMENSION];
+		this.spatial_swarm_center = new double[pso.DIMENSION];
+		this.distances_to_center = new double[pso.NUMBER_OF_PARTICLES];
+		this.distances_among_particles = new double[pso.NUMBER_OF_PARTICLES][pso.NUMBER_OF_PARTICLES];
 	}
 
-	private double euclidianDistance(double[] ds, double[] ds2) {
-		double sum = 0.0f;
-		for (int i = 0; i < ds2.length; i++) {
-			sum += Math.pow(ds[i] - ds2[i], 2);
+	private double euclidianDistance(double[] v1, double[] v2) {
+		double sum = 0.0d;
+		for (int d = 0; d < v2.length; d++) {
+			sum += Math.pow(v1[d] - v2[d], 2);
 		}
 		sum = Math.sqrt(sum);
 		return sum;
 	}
 
-	private void calculateDiameter(){
-
-		double[][] particlesDistance = new double[pso.NUMBER_OF_PARTICLES][pso.NUMBER_OF_PARTICLES];
-		diameter = -1;
-
+	private void evaluateDiameter(){
+		diameter = -1.0;
 		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
 			for (int j = i; j < pso.NUMBER_OF_PARTICLES; j++) {
-				if(i!=j)		
-					particlesDistance[i][j] = euclidianDistance(pso.particle_position[i], pso.particle_position[j]);
-					if(particlesDistance[i][j]>diameter){
-						diameter = particlesDistance[i][j];
+				if (i != j) {		
+					distances_among_particles[i][j] = euclidianDistance(pso.particle_position[i], pso.particle_position[j]);
+					if (distances_among_particles[i][j]>diameter) {
+						diameter = distances_among_particles[i][j];
 					}
+				}
 			}
 		}
-
-//		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-//			for (int j = i; j < pso.NUMBER_OF_PARTICLES; j++) {
-//
-//				if(i!=j && particlesDistance[i][j]>diameter){
-//					diameter = particlesDistance[i][j];
-//				}
-//			}
-//		}
-
 		printer.println("diameter:#"+ this.pso.current_iteration + " " + diameter);
-
 	}
 
-	private void calculateRadius(){
-
-		double[] particlesDistance = new double[pso.NUMBER_OF_PARTICLES];
+	private void evaluateRadius(){
 		radius = -1;
-//		calculateCenterOfSwarm();
 		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-
-			particlesDistance[i] = euclidianDistance(pso.particle_position[i], center);
-		}
-
-		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-
-			if(particlesDistance[i]>radius){
-				radius = particlesDistance[i];
+			if(distances_to_center[i]>radius){
+				radius = distances_to_center[i];
 			}
 		}
-
 		printer.println("radius:#"+ this.pso.current_iteration + " " + radius);
 	}
 
-	private void calculateAvgAroundCenter(){
-		double[] particlesDistance = new double[pso.NUMBER_OF_PARTICLES];
-//		calculateCenterOfSwarm();
+	private void evaluateAverageDistanceAroundCenter(){
 		double sum = 0;
-		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-
-			particlesDistance[i] = euclidianDistance(pso.particle_position[i], center);
+		for (int particle = 0; particle < pso.NUMBER_OF_PARTICLES; particle++) {
+			sum += distances_to_center[particle];
 		}
-
-		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-
-			sum += particlesDistance[i];
-		}
-
-		avgAroundCenter = sum/particlesDistance.length; 
-
-		printer.println("average_around_center:#"+ this.pso.current_iteration +  " " + avgAroundCenter);
+		average_distance_around_center = sum/((float) pso.NUMBER_OF_PARTICLES);
+		normalized_average_distance_around_center = average_distance_around_center/diameter; 
+		printer.println("average_around_center:#"+ this.pso.current_iteration +  " " + average_distance_around_center);
+		printer.println("normalized_average_around_center:#"+ this.pso.current_iteration +  " " + normalized_average_distance_around_center);
 	}
 
-
-
-	private void calculateAvgOfAvg(){
-		double[][] particlesDistance = new double[pso.NUMBER_OF_PARTICLES][pso.NUMBER_OF_PARTICLES];
-		double sum;
-		double[] avg_by_particle = new double[pso.NUMBER_OF_PARTICLES];
-
-		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-			for (int j = i; j < pso.NUMBER_OF_PARTICLES; j++) {
-				if(i!=j)		
-					particlesDistance[i][j] = euclidianDistance(pso.particle_position[i], pso.particle_position[j]);
-
-			}
+	private void evaluateDistancesToCenter() {
+		for (int particle = 0; particle < pso.NUMBER_OF_PARTICLES; particle++) {
+			distances_to_center[particle] = euclidianDistance(pso.particle_position[particle], spatial_swarm_center);
 		}
-
-		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-
-			sum=0;
-			for (int j = i; j < pso.NUMBER_OF_PARTICLES; j++) {
-				if(i!=j)
-					sum += particlesDistance[i][j];		
-			}
-
-			avg_by_particle[i] = sum/pso.NUMBER_OF_PARTICLES;
-		}
-
-		sum=0;
-
-		for (int i = 0; i < pso.NUMBER_OF_PARTICLES; i++) {
-			sum += avg_by_particle[i];
-		}
-
-		avgOfAvg = sum/pso.NUMBER_OF_PARTICLES;
-
-		printer.println("average_of_average_around_all_particles:#"+ this.pso.current_iteration + " " + avgOfAvg);
-
 	}
 
-	private void calculateCenterOfSwarm(){
+	private void evaluateAverageOfAverage(){
 		double sum;
-
-		for(int i=0; i<pso.DIMENSION; i++){
-			sum=0;
-			for(int j=0; j<pso.NUMBER_OF_PARTICLES; j++){
-				sum += pso.particle_position[j][i];
+		double[] average_distance_around_particle = new double[pso.NUMBER_OF_PARTICLES];
+		for (int particle = 0; particle < pso.NUMBER_OF_PARTICLES; particle++) {
+			sum = 0.0;
+			for (int other = 0; other < pso.NUMBER_OF_PARTICLES; other++) {
+				if(particle != other) {
+					sum += distances_among_particles[particle][other];
+				}
 			}
-			center[i] = sum/pso.NUMBER_OF_PARTICLES;
+			average_distance_around_particle[particle] = sum/((float) pso.NUMBER_OF_PARTICLES);
+		}
+		sum = 0.0;
+		for (int particle = 0; particle < pso.NUMBER_OF_PARTICLES; particle++) {
+			sum += average_distance_around_particle[particle];
+		}
+
+		average_distance_among_particles = sum/((float) pso.NUMBER_OF_PARTICLES);
+
+		printer.println("average_of_average_around_all_particles:#"+ this.pso.current_iteration + " " + average_distance_among_particles);
+
+	}
+	
+	private void evaluateSpatialSwarmCenter(){
+		double sum;
+		for(int d = 0; d < pso.DIMENSION; d++){
+			sum = 0.0;
+			for(int particle = 0; particle < pso.NUMBER_OF_PARTICLES; particle++){
+				sum += pso.particle_position[particle][d];
+			}
+			spatial_swarm_center[d] = sum/((float) pso.NUMBER_OF_PARTICLES);
 		}
 	}
 
 	public void iterate(){
-		calculateCenterOfSwarm();
-		calculateDiameter();
-		calculateRadius();
-		calculateAvgAroundCenter();
-		calculateAvgOfAvg();
+		evaluateSpatialSwarmCenter();
+		evaluateDistancesToCenter();
+		evaluateDiameter();
+		evaluateRadius();
+		evaluateAverageDistanceAroundCenter();
+		evaluateAverageOfAverage();
 	}
 }
 
