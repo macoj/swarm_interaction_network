67f6b0631fa6f98d5951143319446e5a7e9627fd
Marcos Oliveira <moliveirajun2013@my.fit.edu>
2017-01-27 19:50:55 -0500

    simulating

--- simulator.py
+++ simulator.py
@@ -78,27 +78,29 @@ class Simulator:
         dimensions = 1000
         particles = 100
         functions = range(21, 29)
-        # for topology in [("kregular", 6)]:
-        #     for function in [1]:
-        #         for k in [5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90]:
-        #             commands += ["java -jar PSO/jar/pso.jar 1 %d %d %d %d %d 0 %d > %s%d_F%02d_%02d " %
-        #                          (particles, evaluations, dimensions, function, topology[1], k,
-        #                          topology[0], k, function, r) for r in range(runs)]
-        #
+        functions = [23]
         runs = 2
-        # for topology in [("global", 0, 0), ("ring", 1, 0), ("regular30", 6, 30)]:
+        for topology in [("kregular", 6)]:
+            for function in [23]:
+                for k in [3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90]:
+                    commands += ["java -jar PSO/jar/pso.jar 1 %d %d %d %d %d 0 %d True > %s%d_F%02d_%02d.with_positions " %
+                                 (particles, evaluations, dimensions, function, topology[1], k,
+                                 topology[0], k, function, r) for r in range(runs)]
+
+        # runs = 2
+        # # for topology in [("global", 0, 0), ("ring", 1, 0), ("regular30", 6, 30)]:
         for topology in [("noc2", 0, 0)]:
-            for function in functions:
+            for function in [23]:
                 commands += ["java -jar PSO/jar/pso_no_c2.jar 1 %d %d %d %d %d 0 %d True > %s_F%02d_%02d.with_positions " %
                              (particles, evaluations, dimensions, function, topology[1], topology[2],
                              topology[0], function, r) for r in range(runs)]
-        runs = 30
-        #for topology in [("global", 0, 0), ("ring", 1, 0), ("regular30", 6, 30)]:
-        for topology in [("noc2", 0, 0)]:
-            for function in functions:
-                commands += ["java -jar PSO/jar/pso_no_c2.jar 1 %d %d %d %d %d 0 %d False > %s_F%02d_%02d.normal " %
-                             (particles, evaluations, dimensions, function, topology[1], topology[2],
-                             topology[0], function, r) for r in range(runs)]
+        # runs = 30
+        # #for topology in [("global", 0, 0), ("ring", 1, 0), ("regular30", 6, 30)]:
+        # for topology in [("noc2", 0, 0)]:
+        #     for function in functions:
+        #         commands += ["java -jar PSO/jar/pso_no_c2.jar 1 %d %d %d %d %d 0 %d False > %s_F%02d_%02d.normal " %
+        #                      (particles, evaluations, dimensions, function, topology[1], topology[2],
+        #                      topology[0], function, r) for r in range(runs)]
         return commands
 
 """
