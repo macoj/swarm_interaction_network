3c4fb25945c253854baae0391d6a2185a88e4fa1
Marcos Oliveira <moliveirajun2013@my.fit.edu>
2017-07-02 16:21:00 -0400

    plotting fitness

--- swarm_plotter.py
+++ swarm_plotter.py
@@ -2,6 +2,8 @@ __author__ = 'marcos'
 from swarm_analyzer import SwarmAnalyzer
 from giant_component_analysis import GiantComponentDeath
 from opt.giant_component_analysis_plotter import GiantComponentDeathPlotter
+from swarm_parser import SwarmParser
+from opt.plotter import Plotter
 
 
 class SwarmPlotter():
@@ -30,3 +32,36 @@ class SwarmPlotter():
     filenames = [('Global', "./data/global_F06_15"), ('Ring', "./data/ring_F06_15"), ('Von Neumann', "./data/vonneumann_F06_15"), ('Dynamic', "./data/dynamicring_F06_15")]
     df = SwarmPlotter.read_files_and_plot_destruction_curves(filenames, windows_size=[10, 50, 100], calculate_on=100, count='size')
     """
+
+    @staticmethod
+    def plot_boxplot_fitness(
+            sets_of_filenames, output_filename=None, info_grep="it\:#", yticks_args=None, legends=None, showmeans=False,
+            showfliers=True, whis=1., widths=0.7, bootstrap=2000, **kargs):
+        """
+        :param sets_of_filenames:
+        :param output_filename:
+        :param info_grep:
+        :return:
+        """
+        get_infos = lambda x: [
+            SwarmParser.read_file_and_measures(filename, informations_grep=info_grep)[1][info_grep][-1][1]
+            for filename in x]
+        values = map(get_infos, sets_of_filenames)
+
+        boxes_kargs = {'color': 'black', 'linewidth': 1.3, 'zorder': 3, 'fillstyle': 'full', 'facecolor': '#a6bddb'}
+        means_kargs = {'color': 'black', 'fillstyle': 'full', 'markerfacecolor': "black", 'marker': "s",
+                       'markersize': 3, 'mew': 1, 'mec': 'black', 'zorder': 5}
+        fliers_kargs = {'color': 'black', 'marker': "s", 'markersize': 1, 'mew': 1.2, 'mec': 'black'}
+        whiskers_kargs = {'color': 'black', 'linewidth': 1.2, 'zorder': 2, 'linestyle': '-', 'alpha': 1.0,
+                          'mec': 'black'}
+        medians_kargs = {'color': 'black', 'linewidth': 1.6, 'zorder': 5, 'alpha': 0.3}
+        caps_kargs = {'linewidth': 1.5, 'color': 'black'}
+        xticks_args = None
+        if legends is not None:
+            xticks_args = (range(1, len(legends)+1), legends)
+        Plotter.plot_boxplots(
+            values, grid_only='y', xlim=(0.3, len(sets_of_filenames) + 0.6), yticks_args=yticks_args, whis=whis,
+            xticks_args=xticks_args, grid=False, widths=widths, bootstrap=bootstrap, boxes_kargs=boxes_kargs,
+            showmeans=showmeans, showfliers=showfliers, fliers_kargs=fliers_kargs, means_kargs=means_kargs,
+            whiskers_kargs=whiskers_kargs, medians_kargs=medians_kargs, caps_kargs=caps_kargs, output=output_filename,
+            **kargs)
