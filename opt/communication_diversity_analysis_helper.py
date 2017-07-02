eaa92b63d785f0143c61e3f2e1d536383465cc20
Marcos Oliveira <moliveirajun2013@my.fit.edu>
2017-07-02 12:01:57 -0400

    helper for calculating communication diversity of many files

--- opt/communication_diversity_analysis_helper.py
+++ opt/communication_diversity_analysis_helper.py
@@ -19,7 +19,26 @@ class CommunicationDiversityHelper():
         if from_ is not None and to_ is not None:
             file_names = file_names[from_:to_]
         for f in file_names:
-            filename = './analysis_on_a_dynamic_topology/%s' % f
+            filename = '.data/analysis_on_a_dynamic_topology/%s' % f
             cd = SwarmAnalyzer.communication_diversity(filename, until=2000)
             cd.to_hdf(filename+"_cd.hdf", 'df')
             del cd
+    """
+execfile("opt/communication_diversity_analysis_helper.py")
+CommunicationDiversityHelper.calculate_communication_diversity(from_=0, to_=10)
+
+execfile("opt/communication_diversity_analysis_helper.py")
+CommunicationDiversityHelper.calculate_communication_diversity(from_=10, to_=20)
+
+execfile("opt/communication_diversity_analysis_helper.py")
+CommunicationDiversityHelper.calculate_communication_diversity(from_=20, to_=30)
+
+execfile("opt/communication_diversity_analysis_helper.py")
+CommunicationDiversityHelper.calculate_communication_diversity(from_=30, to_=40)
+
+execfile("opt/communication_diversity_analysis_helper.py")
+CommunicationDiversityHelper.calculate_communication_diversity(from_=40, to_=50)
+
+execfile("opt/communication_diversity_analysis_helper.py")
+CommunicationDiversityHelper.calculate_communication_diversity(from_=50, to_=60)
+    """
